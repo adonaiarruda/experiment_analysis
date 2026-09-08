8d0bec290c89dd80e6a133b8628a3d6b189ac2c4
alexandrasotoaponte-sketch <alexandrasotoaponte@gmail.com>
2026-09-08 19:49:41 -0300

    teste_normalidade

--- ec1/processamento_separtado.R
+++ ec1/processamento_separtado.R
@@ -28,7 +28,10 @@ calcular_imc_tabela <- function(tabela) {
 }
 
 dados_2016 <- calcular_imc_tabela(dados_2016)
+dados_2016$BMI <- as.numeric(gsub(",", ".", dados_2016$BMI))
+
 dados_2017 <- calcular_imc_tabela(dados_2017)
+dados_2017$BMI <- as.numeric(gsub(",", ".", dados_2017$BMI))
 
 
 fem_2016 <- subset(dados_2016,Gender == "F")
@@ -38,29 +41,60 @@ fem_2017 <- subset(dados_2017,Gender == "F")
 masc_2016 <-subset(dados_2016, Gender == "M")
 masc_2017 <-subset(dados_2017, Gender == "M")
 
-
 fem <- rbind(
-  data.frame(BMI = fem_2016["BMI"], Semestre = "2016-2" ), 
-  data.frame(BMI = fem_2017["BMI"], Semestre = "2017-2" )  
+  data.frame(BMI = fem_2016[["BMI"]], Semestre = "2016-2"),
+  data.frame(BMI = fem_2017[["BMI"]], Semestre = "2017-2")
 )
 
 masc <- rbind(
-  data.frame(BMI = masc_2016["BMI"], Semestre = "2016-2" ), 
-  data.frame(BMI = masc_2017["BMI"], Semestre = "2017-2" )  
+  data.frame(BMI = masc_2016[["BMI"]], Semestre = "2016-2"),
+  data.frame(BMI = masc_2017[["BMI"]], Semestre = "2017-2")
+)
+
+pop_2016 <- rbind(
+  data.frame(BMI = masc_2016[["BMI"]], Gender = "M"),
+  data.frame(BMI = fem_2016[["BMI"]], Gender = "F")
+)
+
+pop_2017 <- rbind(
+  data.frame(BMI = masc_2017[["BMI"]], Gender = "M"),
+  data.frame(BMI = fem_2017[["BMI"]], Gender = "F")
+)
+
+pop_total <- rbind(
+  data.frame(BMI = pop_2016[["BMI"]], Semestre = "2016-2"),
+  data.frame(BMI = pop_2017[["BMI"]], Semestre = "2017-2")
 )
 
 # Caso 1 ------------------------------
 #---------------------------------------
 
-t.test(fem_2016["BMI"],fem_2017["BMI"])
-
 fligner.test(BMI~ Semestre, data = fem)
+t.test(fem_2016$BMI, fem_2017$BMI)
 
 
-t.test(masc_2016["BMI"],masc_2017["BMI"])
 
 fligner.test(BMI~ Semestre, data = masc)
+t.test(masc_2016$BMI, masc_2017$BMI)
 
 
+shapiro.test(pop_2016$BMI)
+shapiro.test(pop_2017$BMI)
+
+shapiro.test(fem_2016$BMI)
+shapiro.test(fem_2017$BMI)
+
+
+shapiro.test(masc_2016$BMI)
+shapiro.test(masc_2017$BMI)
+
+
+t.test(pop_2016$BMI, pop_2017$BMI)
+
+library(car)
+qqPlot(masc_2016$BMI)
+
+library(car)
+qqPlot(pop_2017$BMI)
 
 
